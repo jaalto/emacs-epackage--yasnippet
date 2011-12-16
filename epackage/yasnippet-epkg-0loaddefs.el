647d93e1ebf31c2cc3563eb929872895018e7366
Jari Aalto <jari.aalto@cante.net>
2011-12-16 20:42:01 +0200

    yasnippet-epkg-0loaddefs.el: new

---
+++ epackage/yasnippet-epkg-0loaddefs.el
@@ -0,0 +1,43 @@
+
+;;;### (autoloads (yas/global-mode yas/minor-mode) "yasnippet" "../yasnippet.el"
+;;;;;;  (20201 11324))
+;;; Generated autoloads from ../yasnippet.el
+
+(autoload 'yas/minor-mode "yasnippet" "\
+Toggle YASnippet mode.
+
+When YASnippet mode is enabled, the `tas/trigger-key' key expands
+snippets of code depending on the mode.
+
+With no argument, this command toggles the mode.
+positive prefix argument turns on the mode.
+Negative prefix argument turns off the mode.
+
+You can customize the key through `yas/trigger-key'.
+
+Key bindings:
+\\{yas/minor-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+(defvar yas/global-mode nil "\
+Non-nil if Yas/Global mode is enabled.
+See the command `yas/global-mode' for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `yas/global-mode'.")
+
+(custom-autoload 'yas/global-mode "yasnippet" nil)
+
+(autoload 'yas/global-mode "yasnippet" "\
+Toggle Yas/Minor mode in every possible buffer.
+With prefix ARG, turn Yas/Global mode on if and only if
+ARG is positive.
+Yas/Minor mode is enabled in all buffers where
+`yas/minor-mode-on' would do it.
+See `yas/minor-mode' for more information on Yas/Minor mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+(provide 'yasnippet-epkg-0loaddefs)
