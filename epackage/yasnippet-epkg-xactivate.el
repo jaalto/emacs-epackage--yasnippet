87106fec44ae68ee606a5b2098c435bf2b8414b0
Jari Aalto <jari.aalto@cante.net>
2011-12-19 12:36:25 +0200

    yasnippet-epkg-xactivate.el: rewrite

--- epackage/yasnippet-epkg-xactivate.el
+++ epackage/yasnippet-epkg-xactivate.el
@@ -1,4 +1,15 @@
-(yas/initialize)
-(setq yas/root-directory "~/.emacs.d/epackage/packages/yasnippet/snippets")
-(yas/load-directory yas/root-directory)
+(let ((dir "~/.emacs.d/epackage/packages/yasnippet/snippets"))
+  (cond
+   ((not (file-directory-p dir))
+    (message "[ERROR] yasnippet-epkg-xactivate: Not exists %s" dir))
+   ((or (not (boundp 'yas/root-directory))
+	(not (stringp yas/root-directory)))
+    (setq yas/root-directory dir)
+    (yas/initialize)
+    (yas/load-directory yas/root-directory))
+   ((message
+     (concat "[WARN] yasnippet-epkg-xactivate: Not activating, "
+	     "yas/root-directory already set: %s")
+     yas/root-directory))))
+
 (provide 'yasnippet-epkg-xactivate)
